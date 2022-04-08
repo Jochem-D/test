12b530f7450c7eac88a4085b2da290ac4541d456
Jochem <dijk0146@hz.nl>
2022-04-08 13:34:52 +0200

    commented old code out

--- resources/views/listen.blade.php
+++ resources/views/listen.blade.php
@@ -42,9 +42,7 @@
         const channel = pusher.subscribe('notifications');
         channel.bind('App\\Events\\MessageNotification', function (data){
             poll();
-        })
-        // poll();
-        // window.notifications_poll = setInterval(poll, 15000);
+        });
     </script>
 </body>
 </html>
